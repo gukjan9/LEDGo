ea5b2e6f6008e21f58007ad9aa4b9b36e1675b54
KyeongYoon Kim <39180786+NASA-GukJang@users.noreply.github.com>
2018-11-15 17:05:51 +0900

    Update game.h

--- LEDGoCode/game.h
+++ LEDGoCode/game.h
@@ -25,6 +25,14 @@ int bt5 = 0;
 int bt6 = 0;
 int bt7 = 0;
 
+int ch1 = 48;  
+int ch2 = 50;
+int ch3 = 52;
+
+int swstate1 = 0;
+int swstate2 = 0;
+int swstate3 = 0;
+
 int e1 = 0;
 int e2 = 0;
 int e3 = 0;
@@ -41,13 +49,18 @@ uint32_t C4 = 0x00FFFF; //Cyan
 uint32_t C5 = 0x0000FF; //Blue
 uint32_t C6 = 0xFF00FF; //Magenta
 
-uint32_t MyColors[7] = {C0, C1, C2, C3, C4, C5, C6}; //put the colors in an array
+uint32_t C7 = 0xFF8000; //Orange
+uint32_t C8 = 0x80FF00; //Yellow Green
+uint32_t C9 = 0x0080FF; //Light Blue
+uint32_t C10 = 0x7F00FF; //Purple
 
-uint32_t color2 = MyColors[1];
-uint32_t color1 = MyColors[2];
-uint32_t color3 = MyColors[6];
+uint32_t colors[11] = {C0, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10}; //put the colors in an array
 
-uint32_t colorPlayer1 = color1;
-uint32_t colorPlayer2 = color2;
+uint32_t color2 = colors[1];
+uint32_t color1 = colors[2];
+uint32_t color3 = colors[6];
+
+uint32_t colorPlayer1 = C0;
+uint32_t colorPlayer2 = C0;
 
 #endif 
